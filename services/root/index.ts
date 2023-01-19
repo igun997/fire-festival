2115630b51cbcb210cecf31b0552612d13cd0c93
indra <indra.gunanda@dev.nusatek.id>
2023-01-19 22:19:49 +0700

    fix : deploy

--- services/root/index.ts
+++ services/root/index.ts
@@ -1,33 +1,4 @@
 import { initializeHttp } from '../../configs/http.config';
 import { API_HOST } from '../../constants/http.constant';
-import { RootResources } from '../../types/services/root';
 
 const http = initializeHttp(API_HOST ?? '');
-/**
- * Get Slider Types
- * @param  RootResources.getSliderTypes.request
- * @returns RootResources.getSliderTypes.response
- */
-export const getSliders = (params: RootResources.getSliderTypes.request) => {
-  return http
-    .get('/sliders', { params })
-    .then((response) => response.data as RootResources.getSliderTypes.response)
-    .catch((error) => {
-      throw error;
-    });
-};
-
-/**
- * Get Page Data
- * @param  RootResources.getPageTypes.request
- * @returns RootResources.getPageTypes.response
- */
-
-export const getPage = (params: any) => {
-  return http
-    .get('/pages?populate=*&filters[slug][$eq]=' + params)
-    .then((response) => response.data as RootResources.getPageTypes.response)
-    .catch((error) => {
-      throw error;
-    });
-};
