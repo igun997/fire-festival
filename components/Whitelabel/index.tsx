dfb37f06521dde1a9ca188874ebbfcd7a0d92e73
indra <indra.gunanda@dev.nusatek.id>
2023-01-19 22:29:32 +0700

    remove : old section

--- components/Whitelabel/index.tsx
+++ components/Whitelabel/index.tsx
@@ -1,27 +1,5 @@
-import WelcomeSection from './WelcomeSection';
-import MiniContactSection from './MiniContactSection';
-import InstagramFull from './InstagramFull';
-import PostSection from './PostSection';
-import TextSection from './TextSection';
-import PortfolioSection from './PortfolioSection';
-import ButtonSection from './ButtonSection';
-
 const RenderSection = ({ component, ...props }: any) => {
   switch (component) {
-    case 'welcome_section':
-      return <WelcomeSection {...props} />;
-    case 'mini_contact_section':
-      return <MiniContactSection {...props} />;
-    case 'instagram_full':
-      return <InstagramFull {...props} />;
-    case 'post':
-      return <PostSection {...props} />;
-    case 'text':
-      return <TextSection {...props} />;
-    case 'portfolio_list':
-      return <PortfolioSection {...props} />;
-    case 'button':
-      return <ButtonSection {...props} />;
     default:
       return null;
   }
