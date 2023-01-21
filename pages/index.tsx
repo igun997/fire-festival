1bcaa82228153bb8f6778b8f1edbd89893f9c9a8
indra <indra.gunanda@dev.nusatek.id>
2023-01-21 16:17:46 +0700

    finsih : landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,110 +48,112 @@ const Home: LayoutConfigWithNextPage = (props: any) => {
           span: 12,
           offset: 6,
         }}>
-        <Col
-          xs={24}
-          style={{
-            textAlign: 'center',
-          }}>
-          <Row gutter={[10, 10]} justify="space-between" align="middle">
-            {xs && (
-              <Col xs={24}>
-                <Image
-                  style={{
-                    height: 80,
-                    width: 'auto',
-                    verticalAlign: 'middle',
-                  }}
-                  src={
-                    pathToAsset(
-                      setting?.logo?.data?.attributes?.formats?.small?.url ??
-                        setting?.logo?.data?.attributes?.url,
-                    ) ?? ''
-                  }
-                  preview={false}
-                  width={100}
-                />
-              </Col>
-            )}
-            <Col xs={24}>
-              {!xs ? (
-                <Typography.Title className="landing-heading" level={2}>
-                  <Space direction="horizontal">
-                    <Image
-                      style={{
-                        height: 80,
-                        width: 'auto',
-                        verticalAlign: 'middle',
-                      }}
-                      src={
-                        pathToAsset(
-                          setting?.logo?.data?.attributes?.formats?.small?.url ??
-                            setting?.logo?.data?.attributes?.url,
-                        ) ?? ''
-                      }
-                      preview={false}
-                      width={100}
-                    />
-                    <span>{setting?.site_name ?? ''}</span>
-                  </Space>
-                </Typography.Title>
-              ) : (
-                <Typography.Title className="landing-heading fs-xs" level={2}>
-                  {setting?.site_name ?? ''}
-                </Typography.Title>
+        <Row>
+          <Col
+            xs={24}
+            style={{
+              textAlign: 'center',
+            }}>
+            <Row gutter={[10, 10]} justify="space-between" align="middle">
+              {xs && (
+                <Col xs={24}>
+                  <Image
+                    style={{
+                      height: 80,
+                      width: 'auto',
+                      verticalAlign: 'middle',
+                    }}
+                    src={
+                      pathToAsset(
+                        setting?.logo?.data?.attributes?.formats?.small?.url ??
+                          setting?.logo?.data?.attributes?.url,
+                      ) ?? ''
+                    }
+                    preview={false}
+                  />
+                </Col>
               )}
-            </Col>
-          </Row>
-        </Col>
-        <Row gutter={[10, 10]}>
-          {loadingLanding.loading ? (
-            <Col xs={24}>
-              <Card loading={true} />
-            </Col>
-          ) : (
-            landingData.map((item, index) => (
-              <Col xs={24} key={`${index}-col`} md={8}>
-                <Card onClick={() => navigateTo(item.path, item.is_external)}>
-                  <Row gutter={[5, 5]}>
-                    <Col
-                      xs={24}
-                      style={{
-                        textAlign: 'center',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      }}>
+              <Col xs={24}>
+                {!xs ? (
+                  <Typography.Title className="landing-heading" level={2}>
+                    <Space direction="horizontal">
                       <Image
-                        src={pathToAsset(item?.site_logo?.data?.attributes?.url) ?? ''}
                         style={{
-                          height: 80,
+                          height: 100,
                           width: 'auto',
+                          verticalAlign: 'middle',
                         }}
+                        src={
+                          pathToAsset(
+                            setting?.logo?.data?.attributes?.formats?.small?.url ??
+                              setting?.logo?.data?.attributes?.url,
+                          ) ?? ''
+                        }
                         preview={false}
                       />
-                    </Col>
-                    <Col
-                      xs={24}
-                      style={{
-                        textAlign: 'center',
-                      }}>
-                      <Typography.Text className="landing-title">
-                        {item?.title ?? ''}
-                      </Typography.Text>
-                    </Col>
-                    <Col
-                      xs={24}
-                      style={{
-                        textAlign: 'center',
-                      }}>
-                      <Typography.Text className="landing-desc">
-                        {item?.description ?? ''}
-                      </Typography.Text>
-                    </Col>
-                  </Row>
-                </Card>
+                      <span>{setting?.site_name ?? ''}</span>
+                    </Space>
+                  </Typography.Title>
+                ) : (
+                  <Typography.Title className="landing-heading fs-xs" level={2}>
+                    {setting?.site_name ?? ''}
+                  </Typography.Title>
+                )}
               </Col>
-            ))
-          )}
+            </Row>
+          </Col>
+          <Col xs={24}>
+            <Row gutter={[10, 10]}>
+              {loadingLanding.loading ? (
+                <Col xs={24}>
+                  <Card loading={true} />
+                </Col>
+              ) : (
+                landingData.map((item, index) => (
+                  <Col xs={24} key={`${index}-col`} md={12}>
+                    <Card onClick={() => navigateTo(item.path, item.is_external)}>
+                      <Row gutter={[5, 5]}>
+                        <Col
+                          xs={24}
+                          style={{
+                            textAlign: 'center',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                          }}>
+                          <Image
+                            src={pathToAsset(item?.site_logo?.data?.attributes?.url) ?? ''}
+                            style={{
+                              height: 80,
+                              width: 'auto',
+                            }}
+                            preview={false}
+                          />
+                        </Col>
+                        <Col
+                          xs={24}
+                          style={{
+                            textAlign: 'center',
+                          }}>
+                          <Typography.Text className="landing-title">
+                            {item?.title ?? ''}
+                          </Typography.Text>
+                        </Col>
+                        <Col
+                          xs={24}
+                          style={{
+                            textAlign: 'center',
+                          }}>
+                          <Typography.Text className="landing-desc">
+                            {item?.description ?? ''}
+                          </Typography.Text>
+                        </Col>
+                      </Row>
+                    </Card>
+                  </Col>
+                ))
+              )}
+            </Row>
+          </Col>
         </Row>
       </Col>
     </Row>
